b21b6f1ea355daf8b46a3a4bb88bffb2cc579eb0
Lee Hyeon Ju <sinbihae@gmail.com>
2021-06-15 17:40:00 +0900

    [UPDATE] start 메시지 수정

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,6 @@ async function bootstrap() {
   // app.useGlobalPipes(new ValidationPipe())  // validationPipe 확인
 
   await app.listen(3000);
-  // console.log(`Application is running on: ${await app.getUrl()}`)
+  console.log(`Application is running on: :3000`)
 }
 bootstrap();
